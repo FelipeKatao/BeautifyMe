5ecf8e141075c18eaea192958fb217fcde6e993b
stephaniedelgado <stephanie-delgado@outlook.com.br>
2019-04-30 11:49:41 -0300

    fxs

---
+++ src/view/Home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Badge } from 'react-bootstrap';
+
+class Home extends React.Component {
+    render() {
+      return (
+      <Badge variant="primary">New</Badge>
+      );
+    }
+}
+export default Home;
